1469e18afb4672440a599fc2cd115810a4c3a328
temirugur <43929561+temirugur@users.noreply.github.com>
2018-12-21 23:59:06 +0300

    Update GUIMain.java

--- GUIMain.java
+++ GUIMain.java
@@ -11,3 +11,12 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+
+public class GUIMain {
+	
+	private static Software software;
+	public static void main(String []args) {
+	software = new Software();
+	JFrame mainFrame = new JFrame("DPS");
+	mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	mainFrame.setSize(800,600);
