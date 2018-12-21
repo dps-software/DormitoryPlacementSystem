554af06d186fbd8319d77b44a20e47f823a9ce12
onaybasibuyuk <43809941+onaybasibuyuk@users.noreply.github.com>
2018-12-21 23:11:52 +0300

    Update RegisteredList.java

--- RegisteredList.java
+++ RegisteredList.java
@@ -4,16 +4,5 @@ import java.util.List;
 public class RegisteredList {
 	List<Student> registeredList = new ArrayList<Student>();
 	
-	public RegisteredList(){
-		registeredList = new ArrayList<Student>(250);
-	}
 	
-	public void addStudent(Student student){
-		registeredList.add(student);
-	}
-	
-	public void removeStudent(Student student){
-		registeredList.remove(student);
-	}
-
 }
